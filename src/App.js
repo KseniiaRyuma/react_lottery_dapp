a0a706ce1ff0257855197a7541405dc4eede08d2
KseniiaRyuma <kseniya0213@gmail.com>
2018-04-09 23:15:02 -0700

    interactive front-end

--- src/App.js
+++ src/App.js
@@ -5,21 +5,49 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  };
 
-    this.state = { manager: '' };
-  }
   async componentDidMount() {
     const manager = await lottery.methods.manager().call(); // working with metamask provider
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const accounts = await web3.getAccount();
+  };
+
   render() {
     return (
       <div>
-        <h1>LOTTERY</h1>
-        <p>This contract is managed by {this.state.manager}</p>
+        <h1>LOTTERY CONTRACT</h1>
+        <p>
+          This contract is managed by {this.state.manager}. There are currently{' '}
+          {this.state.players.length} people entered, competing to win{' '}
+          {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4> Want to try your luck? </h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
